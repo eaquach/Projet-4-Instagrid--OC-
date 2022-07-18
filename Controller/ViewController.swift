22bd563817a586c6c72cc3bd0ebe8a034ca3e414
Elizabeth Quach <e.quach@hotmail.fr>
2022-07-18 13:53:59 +0200

    ImagePickerController review code
    review code imagePickerController

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 
 
 class ViewController: UIViewController {
-   
-
+    
+    
     @IBOutlet weak var PictureStackView: UIStackView!
     
     
@@ -21,51 +21,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    
     @IBOutlet var plusButton: [UIButton]!
     @IBOutlet var layoutButton: [UIButton]!
-   
     
     
-  
+    
+    
     
     
     @IBAction func layoutButtonTapped(_ sender: UIButton) {
         let tag = sender.tag
-                for button in layoutButton {
-                    if button.tag == tag {
-                        // selected
-                        button.setTitle("", for: .normal)
-        
-                        let image = UIImage(named: "Selected")
-                        button.setImage(image, for: .normal)
-                    } else {
-                        let image = UIImage()
-                        button.setImage(image, for: .normal)
-                    }
-        
-                    }
-    
-        sender.isSelected = true
-        
-        switch sender.tag {
-        case 0 :
-            plusButton[0].isHidden = true
-            plusButton[3].isHidden = false
-        case 1 :
-            plusButton[0].isHidden = false
-            plusButton[3].isHidden = true
-            
-            
-        case 2 :
-            plusButton[0].isHidden = false
-            plusButton[3].isHidden = false
-        default:
-            break
+        for button in layoutButton {
+            if button.tag == tag {
+                // selected
+                button.setTitle("", for: .normal)
+                
+                let image = UIImage(named: "Selected")
+                button.setImage(image, for: .normal)
+            } else {
+                let image = UIImage()
+                button.setImage(image, for: .normal)
+            }
             
         }
         
-}
+        sender.isSelected = true
+        
+        switch sender.tag {
+            case 0 :
+                plusButton[0].isHidden = true
+                plusButton[3].isHidden = false
+            case 1 :
+                plusButton[0].isHidden = false
+                plusButton[3].isHidden = true
+                
+                
+            case 2 :
+                plusButton[0].isHidden = false
+                plusButton[3].isHidden = false
+            default:
+                break
+                
+        }
+        
+    }
+    
+    
     
     @IBAction func didTapButton() {
         let vc = UIImagePickerController()
@@ -74,27 +76,31 @@ class ViewController: UIViewController {
         vc.allowsEditing = true
         present(vc, animated: true)
     }
-  
-            
-          
-            
-            }
-     
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        print("\(info)")
-        picker.dismiss(animated: true)
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true)
-    }
+    
+    
+    
 }
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            plusButton[0].setImage(image, for: .normal)
+            
+        }
+        
+        picker.dismiss(animated: true)
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+    }
     
     
     
-
-
-
+    
+    
+    
 }
 
